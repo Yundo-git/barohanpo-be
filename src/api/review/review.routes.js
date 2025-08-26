51306a5270ea0ed08e96b299b5f300c0d4e4b90f
ydh <ydh7804@gmail.com>
2025-08-26 23:28:40 +0900

    review router 생성

---
+++ src/api/review/review.routes.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const { getAllReviews, getReviewById, createReview, updateReview, deleteReview } = require("./review.controller");
+
+const router = express.Router();
+
+router.get("/", getAllReviews);
+router.get("/:id", getReviewById);
+router.post("/", createReview);
+router.put("/:id", updateReview);
+router.delete("/:id", deleteReview);
+
+module.exports = router;
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ const pharmacyRoutes = require("../api/pharmacy/pharmacy.routes");
 const pha_userRoutes = require("../api/pha_user/pha_user.routes");
 const booksRoutes = require("../api/reservation/reservation.routes");
 const authRoutes = require("../api/auth/auth.routes");
-
+const reviewRoutes = require("../api/review/review.routes");
 const router = express.Router();
 
 // 라우터정의
@@ -11,5 +11,6 @@ router.use("/pharmacy", pharmacyRoutes); //약국 관련 라우터
 router.use("/pha_user", pha_userRoutes); //약사 관련 라우터
 router.use("/reservation", booksRoutes); //예약 관련 라우터
 router.use("/auth", authRoutes); //회원 관련 라우터
+router.use("/review", reviewRoutes); //리뷰 관련 라우터
 //env에 있는 API_PREFIX를 사용하여 라우터를 정의필요
 module.exports = router;
